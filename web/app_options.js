2993da64e1eebea4dc65bef93f31e881d55b8bd0
Long Thieu <longthieu@MacBookAir.lan>
2025-08-14 20:59:17 +0900

    addurl

--- web/app_options.js
+++ web/app_options.js
@@ -533,7 +533,7 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
     value:
       typeof PDFJSDev !== "undefined" && PDFJSDev.test("CHROME")
         ? ""
-        : "compressed.tracemonkey-pldi-09.pdf",
+        : PDF_URL, // rejob
     kind: OptionKind.VIEWER,
   };
   defaultOptions.sandboxBundleSrc = {
