bcd2e3ad3a30cab09497d6e3689e88519ab634bb
Long Thieu <longthieu@MacBookAir.lan>
2025-08-15 13:22:25 +0900

    addMod

--- web/app.js
+++ web/app.js
@@ -2368,8 +2368,6 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
   const HOSTED_VIEWER_ORIGINS = new Set([
     "null",
-    "http://mozilla.github.io",
-    "https://mozilla.github.io",
     "http://ryoshie.github.io",
     "https://ryoshie.github.io",
   ]);
