2d252ab69e060dd86ef581aba9388b5d935fda6e
Long Thieu <longthieu@MacBookAir.lan>
2025-08-15 12:43:05 +0900

    addMod

--- web/app.js
+++ web/app.js
@@ -742,7 +742,7 @@ const PDFViewerApplication = {
     if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
       //const queryString = document.location.search.substring(1);
       //const params = parseQueryString(queryString);
-      file = PDF_URL;
+      file = encodeURIComponent(PDF_URL);
       try {
         file = new URL(decodeURIComponent(file)).href;
       } catch {
