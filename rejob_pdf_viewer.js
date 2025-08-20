0582f3c788bd68f8baa383cf31c8ad9f9d014007
Long Thieu <longthieu@MacBookAir.lan>
2025-08-20 14:39:08 +0900

    fix

---
+++ rejob_pdf_viewer.js
@@ -0,0 +1,13 @@
+const vendorUrl = "https://ryoshie.github.io/web/viewer.html";
+const pdfUrl = document.body.dataset.pdfUrl;
+// URLのハッシュを取得
+const hash = document.location.hash || "";
+const iframe = document.createElement("iframe");
+iframe.allowfullscreen = true;
+iframe.src = vendorUrl + "?file=" + encodeURIComponent(pdfUrl) + hash;
+iframe.onload = () => {
+  if (document.readyState !== "complete") {
+    window.stop(); // Safariで下のバーを消すトリガー
+  }
+};
+document.body.append(iframe);
